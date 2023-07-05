69502998f901e1d3be8caa5972ae8082132322c9
Hayley <hayleymdean271@gmail.com>
2023-07-05 13:05:30 -0700

    adding home page view

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 //Controller
 app.use('/places', require('./controllers/places'))
 
 //Routes
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
