9b3dfe7da8a0c2dc3c2934c526278a3748aa0e88
Hayley <hayleymdean271@gmail.com>
2023-07-05 15:10:04 -0700

    added images

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,8 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <img src="/images/sad_pug.jpg" alt="sad pug" />
+                Photo by <a href="https://unsplash.com/@matthewhenry?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Matthew Henry</a> on <a href="https://unsplash.com/s/photos/sad-dog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
             </main>
         </Def>
     )
