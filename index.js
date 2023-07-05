1f98d13e872c4b06b63b933cea540485bdb384a6
Hayley <hayleymdean271@gmail.com>
2023-07-05 12:53:39 -0700

    Adding default view for layout

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Controller
 app.use('/places', require('./controllers/places'))
 
+//Routes
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
