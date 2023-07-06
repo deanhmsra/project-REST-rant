efb5cb9755ed06496e43c8552be2db7f0490d5cb
Hayley <hayleymdean271@gmail.com>
2023-07-05 17:22:17 -0700

    Addind and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>REST-Rant</h1>
                 <div>
                     <img src="./images/veggie-bowl.jpg" alt="Bowl of Vegetables" />
                     <div>
