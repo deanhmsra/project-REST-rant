5327eae81ed393f6ad61003243cd69dc833930f3
Hayley <hayleymdean271@gmail.com>
2023-07-11 17:11:31 -0700

    Show route connected

---
+++ views/places/show.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function show () {
+    return (
+        <Def>
+            <main>
+                <h2>Show page</h2>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = show
